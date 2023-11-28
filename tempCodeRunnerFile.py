2fd6a99b40f29cd54ebac5286c6c5276f735f80d
Joel T. Jayme <threadjayme@gmail.com>
2023-11-28 22:49:08 +0800

    debug |

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-        # print(page_list[i], end="")
+ print(f"[{frame_content}|", end="") if frame_content != next_to_replace else print(f"[{frame_content}-", end="")
+            # elif j == frame_num - 1:
+            #     print(f"{frame_content}] ", end="")
+            # else:
+            #     print(f"{frame_content}", end="")
